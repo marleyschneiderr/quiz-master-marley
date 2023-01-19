30e273dbec214c900f074b07a077616822a9c486
marleyschneiderr <marley33@uw.edu>
2023-01-19 13:16:07 -0800

    Adding HTML and connecting first part of JavaScript timer

---
+++ assets/javascript/script.js
@@ -0,0 +1,37 @@
+var gamescore = 0;
+var questionsVariable = 0;
+var startScreen = 
+
+
+
+
+
+
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+    // Add an event listener for the start button
+let start = document.querySelector(#button-start);
+
+start.addEventListener ("click", function()
+    var timeEl = document.querySelector(".timer");
+    var secondsLeft = 75;
+
+
+
+
+
+
+
+)
+
+// WHEN I answer a question
+// THEN I am presented with another question
+
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+
+// WHEN the game is over
+// THEN I can save my initials and my score
