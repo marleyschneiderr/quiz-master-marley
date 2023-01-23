d598f069a0173d0f084ccf0294310a66bbe6fcb4
marleyschneiderr <marley33@uw.edu>
2023-01-22 17:52:51 -0800

    Adding new HTML and CSS styling

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,15 +1,15 @@
-var gamescore = 0;
-var presentQuestion = 0;
-var startScreen = 0;
-var pauseScreen = 0;
-var secondsRemaining = 75;
-var incorrect = 10;
+// var gamescore = 0;
+ // var presentQuestion = 0;
+// var startScreen = 0;
+// var pauseScreen = 0;
+// var secondsRemaining = 75;
+// var incorrect = 10;
 
 // buttons that will be clicked on screen
 
-var ClickStartScreen = document.getElementById('Start-Game');
-var ClickNextButton = document.getElementById('next-button');
-var ClickStartButton = document.getElementById('button-start');
+// var ClickStartScreen = document.getElementById('Start-Game');
+// var ClickNextButton = document.getElementById('next-button');
+// var ClickStartButton = document.getElementById('button-start');
 
 
 
@@ -17,52 +17,52 @@ var ClickStartButton = document.getElementById('button-start');
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
 
-const startButton = document.getElementById("start-button");
-    startButton.addEventListener("click", startQuiz);
+// const startButton = document.getElementById("start-button");
+ //    startButton.addEventListener("click", startQuiz);
 
 //startQuiz function that will start the timer, present the 1st question, bind click event on options
-function startQuiz() {
+  // function startQuiz() {
     // start timer
-    startTimer();
+  //  startTimer();
 
     // 1st question shows up
-    presentQuestion();
+  //  presentQuestion();
 
     //bind clikc event on options
-    bindOptionClickEvent();
-}
+  //  bindOptionClickEvent();
+// }
 
 // start timer and update the timer display on the page
-function startTimer() {
-    let time = 75;
-    const timerInterval = setInterval(function() {
-        time --;
-        document.getElementById("timer").innerHTML = time;
-        if (time === 0) {
-            clearInterval(timerInterval);
-        }
-    }, 1000);
-}
+// function startTimer() {
+ //   let time = 75;
+ //   const timerInterval = setInterval(function() {
+ //       time --;
+      //  document.getElementById("timer").innerHTML = time;
+//      if (time === 0) {
+   //         clearInterval(timerInterval);
+ //       }
+   //  }, 1000);
+// }
 
 // present the first question and options on the page
-function presentQuestion() {
-    const question = "Inside which HTML element do we put the JavaScript?";
-    const options = ["<script>", "<js>", "<scripting>", "<javascript>"]
-}
+// function presentQuestion() {
+  //  const question = "Inside which HTML element do we put the JavaScript?";
+   // const options = ["<script>", "<js>", "<scripting>", "<javascript>"]
+// }
 
-function bindOptionClickEvent() {
-    const options = document.querySelectorAll(".option");
-    options.forEach(function(option) {
-        option.addEventListener("click", function() {
-            // Check if the selected option is right
-            if (this.innerHTML === "<script>") {
-                console.log("CORRECT!");
-            } else {
-                console.log("WRONG!");
-            }
-        });
-    });
-}
+// unction bindOptionClickEvent() {
+   //  const options = document.querySelectorAll(".option");
+  //  options.forEach(function(option) {
+   //     option.addEventListener("click", function() {
+   //         // Check if the selected option is right
+   //         if (this.innerHTML === "<script>") {
+   //             console.log("CORRECT!");
+   //         } else {
+   //             console.log("WRONG!");
+   //         }
+  //      });
+  //  });
+// }
 
 // WHEN I answer a question
 // THEN I am presented with another question
@@ -75,3 +75,23 @@ function bindOptionClickEvent() {
 
 // WHEN the game is over
 // THEN I can save my initials and my score
+
+
+
+
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+
+// quiz variables 
+
+
