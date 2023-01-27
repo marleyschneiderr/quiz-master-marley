8f92b775b809cfafe41d2052140716328e357e7f
marleyschneiderr <marley33@uw.edu>
2023-01-26 20:42:49 -0800

    Adding attempted timer

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,99 +1,3 @@
-// var gamescore = 0;
- // var presentQuestion = 0;
-// var startScreen = 0;
-// var pauseScreen = 0;
-// var secondsRemaining = 75;
-// var incorrect = 10;
-
-// buttons that will be clicked on screen
-
-// var ClickStartScreen = document.getElementById('Start-Game');
-// var ClickNextButton = document.getElementById('next-button');
-// var ClickStartButton = document.getElementById('button-start');
-
-
-
-
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-
-// const startButton = document.getElementById("start-button");
- //    startButton.addEventListener("click", startQuiz);
-
-//startQuiz function that will start the timer, present the 1st question, bind click event on options
-  // function startQuiz() {
-    // start timer
-  //  startTimer();
-
-    // 1st question shows up
-  //  presentQuestion();
-
-    //bind clikc event on options
-  //  bindOptionClickEvent();
-// }
-
-// start timer and update the timer display on the page
-// function startTimer() {
- //   let time = 75;
- //   const timerInterval = setInterval(function() {
- //       time --;
-      //  document.getElementById("timer").innerHTML = time;
-//      if (time === 0) {
-   //         clearInterval(timerInterval);
- //       }
-   //  }, 1000);
-// }
-
-// present the first question and options on the page
-// function presentQuestion() {
-  //  const question = "Inside which HTML element do we put the JavaScript?";
-   // const options = ["<script>", "<js>", "<scripting>", "<javascript>"]
-// }
-
-// unction bindOptionClickEvent() {
-   //  const options = document.querySelectorAll(".option");
-  //  options.forEach(function(option) {
-   //     option.addEventListener("click", function() {
-   //         // Check if the selected option is right
-   //         if (this.innerHTML === "<script>") {
-   //             console.log("CORRECT!");
-   //         } else {
-   //             console.log("WRONG!");
-   //         }
-  //      });
-  //  });
-// }
-
-// WHEN I answer a question
-// THEN I am presented with another question
-
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-
-// WHEN the game is over
-// THEN I can save my initials and my score
-
-
-
-
-
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and my score
-
-// quiz variables 
-
 // select items to target 
 // using document query selector - it can target both class and ids
 const question = document.querySelector('#question');
@@ -101,6 +5,7 @@ const answers = Array.from(document.querySelectorAll('.answer-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
+// const startButtonEl = document.getElementById('start-button');
 
 // create let variables for current question selection
 let currentQuestion = 0;
@@ -108,10 +13,7 @@ let acceptingAnswers = true;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
-
-// timer 
-var timeEl = document.querySelector("#timer");
-
+ 
 // array with question objects in it
 let questions = [ {
     question: 'Which of the following keywords is used to define a variable in JavaScript?',
@@ -174,11 +76,30 @@ const MAX_QUESTIONS = 6;
 
 // start game function
 startGame = () => {
+    startTimer()
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
     getNewQuestion();
 }
+
+// timer 
+startGame.addEventListener("click", function () {
+    // We are checking zero because its originally set to zero
+    if (pauseInterval === 0) {
+        pauseInterval = setInterval(function () {
+            secondsLeft--;
+            timeEl.textContent = "Time: " + secondsLeft;
+
+            if (secondsLeft <= 0) {
+                clearInterval(pauseInterval);
+                allDone();
+                timeEl.textContent = "Time's up!";
+            }
+        }, 1000);
+    }
+  //  mainScreen(currentQuestion);
+});
 
 // function to get new question
 getNewQuestion = () => {
