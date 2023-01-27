d889ec9ee9de02bef3bbd2dc295a8cc2552e79fe
marleyschneiderr <marley33@uw.edu>
2023-01-27 01:09:30 -0800

    Fixing a ton of errors, you can now see names populate on leaderboard

--- assets/javascript/end.js
+++ assets/javascript/end.js
@@ -1,8 +1,8 @@
 // define variables for end page js
-const username = document.querySelector('#username');
-const saveScoreBtn = document.querySelector('#saveScoreBtn');
-const finalScore = document.querySelector('#finalScore');
-const mostRecentScore = localStorage.getItem('mostRecentScore');
+const username = document.querySelector("#username");
+const saveScoreBtn = document.querySelector("#saveScoreBtn");
+const finalScore = document.querySelector("#finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
 
 // highscore page js variables
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
@@ -12,27 +12,28 @@ const MAX_HIGH_SCORES = 5;
 finalScore.innerText = mostRecentScore;
 
 // whenever a user presses the key, it is going to re-enable the save function for the highscore name inputed 
-username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.value;
-})
+function saveHighScore() {
+    if (username.value !== "") {
+        const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+        const score = {
+            score: mostRecentScore,
+            name: username.value,
+        };
 
-// allows user to click button, page will not refresh unless button is clicked 
-saveHighScore = e => {
-    e.preventDefault()
+        highScores.push(score);
 
-    const score = {
-        score: mostRecentScore,
-        name: username.value
-    };
-    
-    highScores.push(score);
+        highScores.sort((a, b) => {
+            return b.score - a.score;
+        });
 
-    highScores.sort((a,b) => {
-        return b.score - a.score;
-    })
+        highScores.splice(5);
 
-    highScores.splice(5);
+        localStorage.setItem("highScores", JSON.stringify(highScores));
+        window.location.href = "./index.html";
+    }
+}
 
-    localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
-};
+saveScoreBtn.addEventListener("click", saveHighScore);
+
+
+   
--- assets/javascript/highscores.js
+++ assets/javascript/highscores.js
@@ -1,8 +1,9 @@
-const highScoresList = document.querySelector('#highScoresList');
-const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+const highScoresList = document.querySelector("#highScoresList");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 // allows the score you save to show up on screen, whatever you type in will fill it in
-highScoresList.innerHTML = 
-highScores.map(socre => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`
-}).join('');
+highScoresList.innerHTML = highScores
+.map((score) => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+})
+.join("");
